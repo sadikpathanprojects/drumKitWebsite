8326637fd83fca5a3876b4b8639fabdee73045d5
sadikpathan9876 <65956063+sadikpathanprojects@users.noreply.github.com>
2021-11-28 12:57:17 +0530

    Add files via upload

---
+++ js/file.js
@@ -0,0 +1,48 @@
+document.querySelector(".btn1").addEventListener("click",function(){
+
+    var audio1=new Audio("sounds/crash.mp3");
+    audio1.play();
+    
+    
+    });
+
+    document.querySelector(".btn2").addEventListener("click",function(){
+
+        var audio1=new Audio("sounds/kick-bass.mp3");
+        audio1.play();
+        
+        
+        });
+    
+
+        document.querySelector(".btn3").addEventListener("click",function(){
+
+            var audio1=new Audio("sounds/snare.mp3");
+            audio1.play();
+            
+            
+            });
+
+            document.querySelector(".btn4").addEventListener("click",function(){
+
+                var audio1=new Audio("sounds/tom-1.mp3");
+                audio1.play();
+                
+                
+                });
+
+                document.querySelector(".btn5").addEventListener("click",function(){
+
+                    var audio1=new Audio("sounds/tom-2.mp3");
+                    audio1.play();
+                    
+                    
+                    });
+
+                    document.querySelector(".btn6").addEventListener("click",function(){
+
+                        var audio1=new Audio("sounds/tom-3.mp3");
+                        audio1.play();
+                        
+                        
+                        });
